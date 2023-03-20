1cfc4280c59c8f4f71a2ccdcae68ccf0f8498680
Teresa <tenjerim@gmail.com>
2023-03-20 21:07:35 +0300

    completed

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,5 +1,7 @@
 class ToysController < ApplicationController
   wrap_parameters format: []
+  rescue_from ActiveRecord::RecordInvalid, with: :render_response_unprocessable_entity
+
 
   def index
     toys = Toy.all
@@ -12,20 +14,28 @@ class ToysController < ApplicationController
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    toy = find_toy
+    toy.update!(toy_params)
+    render json: toy, status: :accepted
   end
 
   def destroy
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.destroy
     head :no_content
   end
 
   private
-  
+
   def toy_params
     params.permit(:name, :image, :likes)
   end
 
+  def find_toy
+    toy = Toy.find_by(id: params[:id])
+  end
+
+  def render_response_unprocessable_entity(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
